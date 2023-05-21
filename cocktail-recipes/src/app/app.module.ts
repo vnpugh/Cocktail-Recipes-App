b8448d53cddc9d8329164091f6bc2be312f1f245
vnpugh <vnp19@yahoo.com>
2023-05-21 12:01:32 -0500

    Made changes to service and module files

--- cocktail-recipes/src/app/app.module.ts
+++ cocktail-recipes/src/app/app.module.ts
@@ -6,19 +6,25 @@ import { CocktailsListComponent } from './cocktails-list/cocktails-list.componen
 import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
 import { ClarityModule } from '@clr/angular';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NotFoundPageComponent } from './not-found-page/not-found-page.component';
 
 // Define your routes
 const routes: Routes = [
   { path: 'list', component: CocktailsListComponent },
-  { path: 'details', component: CocktailDetailsComponent },
-  { path: '', pathMatch: 'full', redirectTo: 'list' } // Add a default redirect path
+
+  { path: 'cocktail-details/:id', component: CocktailDetailsComponent },
+
+  { path: '', pathMatch: 'full', redirectTo: 'list' }, // Add a default redirect path
+
+  { path: '**', component: NotFoundPageComponent }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     CocktailsListComponent,
-    CocktailDetailsComponent
+    CocktailDetailsComponent,
+    NotFoundPageComponent
   ],
   imports: [
     BrowserModule,
--- cocktail-recipes/src/app/cocktail-details/cocktail-details.component.ts
+++ cocktail-recipes/src/app/cocktail-details/cocktail-details.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-cocktail-details',
   templateUrl: './cocktail-details.component.html',
   styleUrls: ['./cocktail-details.component.css']
 })
-export class CocktailDetailsComponent {
+export class CocktailDetailsComponent implements OnInit {
+  id!: string | null;
 
+  constructor(private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.activatedRoute.paramMap.pipe(
+      switchMap(paramsMap => of(this.id = paramsMap.get('id')))
+    ).subscribe();
+  }
 }
